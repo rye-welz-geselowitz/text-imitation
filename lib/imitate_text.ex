6baac6c20e120bd9682ebb1292b5199a69631a09
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2018-02-27 23:08:10 -0500

    more cleanup

--- lib/imitate_text.ex
+++ lib/imitate_text.ex
@@ -1,23 +1,37 @@
 defmodule ImitateText do
   @moduledoc """
-  Documentation for ImitateText.
+  Uses Markov Chain to generate imitations of input text
   """
 
   @doc """
+  Generates imitations of input text. Takes
+  * text - a string to be imitated
+  * word_count - integer representing the desired length of the imitation
+  * options - array of configurable options
+    - :strip_punctuation - boolean determining whether the function should
+      strip punctuation before processing the text. Default false.
 
+  ### Examples
+  iex> text = "I love her but I don't think she knows that I love her. I think she loves me, but does she know she loves me?"
+  iex> ImitateText.generate(text, 5)
+  "HER. I LOVE HER. I LOVE HER BUT I LOVE"
 
   """
   # # # EXPOSED # # #
-  def imitate_text(text, word_count, options \\ []) do
+  def generate(text, word_count, options \\ []) do
     word_list = get_word_list(text, options)
-    words_to_followers = build_frequency_dictionary(word_list)
-    generate_chain(words_to_followers, word_count)
+    frequency_table = build_frequency_table(word_list)
+    generate_chain(frequency_table, word_count)
   end
 
 
   # # # PRIVATE # # #
 
-  # word list
+  # - word list -
+
+  # Helper function:
+  # Splits a text on whitespace and newlines. Strips punctuation if
+  # specified in options.
   defp get_word_list(text, options) do
     strip_punctuation? = Keyword.get(options, :strip_punctuation, false)
     text
@@ -25,6 +39,10 @@ defmodule ImitateText do
     |> String.split(~r{[^\S]}) |> Enum.map(&clean(&1))
   end
 
+  # Helper function:
+  # Takes a text and boolean strip_punctuation?
+  # If strip_punctuation? is true, returns text without punctuation
+  # Otherwise returns text unaltered
   defp handle_punctuation(text, strip_punctuation?) do
     if strip_punctuation? do
       text |> String.replace(~r{([^a-zA-Z])([\s\r])}, "\\g{2}", [global: true])
@@ -33,12 +51,23 @@ defmodule ImitateText do
     end
   end
 
+  # Helper function:
+  # Trims and upcases a word
   defp clean(word) do
     word |> String.trim() |> String.upcase()
   end
 
-  # frequency dictionary
-  defp build_frequency_dictionary(word_list) do
+  #  - frequency table -
+  # Helper function:
+  # Takes a list of word and produces a map whose keys are the words
+  # and whose values are maps, mapping the words that follow the original
+  # word to their frequencies
+  # e.g.
+  #  %{
+  #   "BUT" => %{"DOES" => 1, "I" => 2},
+  #   "DOES" => %{"SHE" => 1} ...}
+  # indicates that "BUT" is followed once by "DOES" and twice by "I"
+  defp build_frequency_table(word_list) do
     word_list
     |> Enum.with_index()
     |> Enum.reduce(%{}, fn {word, idx}, dict ->
@@ -51,21 +80,29 @@ defmodule ImitateText do
     end)
   end
 
-  # build chain
-  defp generate_chain(words_to_followers, remaining_word_count, prev_word \\ nil) do
+  # - build chain -
+
+  # Helper function:
+  # Recursively builds a chain of words. To select a follower for a given word,
+  # assigns probablities to words observed to follow that word in the original
+  # text, based on their frequency.
+  defp generate_chain(frequency_table, remaining_word_count, prev_word \\ nil) do
     if remaining_word_count < 1 do
       ""
     else
-      word = get_next_word(words_to_followers, prev_word)
-      word <> " " <> generate_chain(words_to_followers, remaining_word_count - 1, word)
+      word = get_next_word(frequency_table, prev_word)
+      word <> " " <> generate_chain(frequency_table, remaining_word_count - 1, word)
       |> String.trim
     end
   end
 
-  defp get_next_word(words_to_followers, prev_word) do
-    case words_to_followers[prev_word] do
+  # Helper function:
+  # Chooses the next using the frequency table. If no candidate words can be
+  # found, randomly chooses a word from the text.
+  defp get_next_word(frequency_table, prev_word) do
+    case frequency_table[prev_word] do
       nil ->
-        random_restart(words_to_followers)
+        random_restart(frequency_table)
 
       freq_map ->
         {k, _} = Enum.max_by(freq_map, fn {_k, v} -> v * :rand.uniform(10) end)
@@ -73,9 +110,12 @@ defmodule ImitateText do
     end
   end
 
-  defp random_restart(words_to_followers) do
-    Map.keys(words_to_followers)
-    |> Enum.at(:rand.uniform(Enum.count(words_to_followers)))
+  # Helper function:
+  # Chooses a random key from the frequency table, i.e. a random word from
+  # the original text
+  defp random_restart(frequency_table) do
+    Map.keys(frequency_table)
+    |> Enum.at(:rand.uniform(Enum.count(frequency_table)-1))
   end
 
 end
